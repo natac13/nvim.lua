0e418efa53c399df1bcb4403ea016e8ed20ec995
Sean Campbell <sean.campbell13@gmail.com>
2023-04-18 19:41:44 -0400

    highlight yanked

--- lua/natac/keymaps.lua
+++ lua/natac/keymaps.lua
@@ -55,3 +55,23 @@ keymap.set("n", "j", 'v:count == 0 ? "gj" : "j"', { expr = true, silent = true }
 
 keymap.set("v", "<C-j>", ":m '>+1<CR>gv=gv")
 keymap.set("v", "<C-k>", ":m '<-2<CR>gv=gv")
+
+-- next greatest remap ever : asbjornHaland
+-- ability to yank to system clipboard
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup("HighlightYank", {})
+
+autocmd("TextYankPost", {
+	group = yank_group,
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank({
+			higroup = "IncSearch",
+			timeout = 100,
+		})
+	end,
+})
